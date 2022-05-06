f5b8d1eecbda0444fe52ca730186e28aab95d201
Celestine Akpanoko <celestineakpanoko@gmail.com>
2022-05-06 21:36:17 +0100

    fix error

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -3,9 +3,15 @@
 
 from base_caching import BaseCaching
 
+from collections import OrderedDict
+
 
 class FIFOCache(BaseCaching):
     """A FIFO caching system inherited from BaseCache"""
+    def __init__(self):
+        """Initialization"""
+        super().__init__()
+        self.cache_data = OrderedDict()
 
     def put(self, key, item):
         """ Using FIFO, assigns value to self.cache_data with key"""
@@ -13,8 +19,8 @@ class FIFOCache(BaseCaching):
             pass
         self.cache_data[key] = item
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            print("DISCARD: ", list(self.cache_data.keys())[0])
-            self.cache_data.pop(list(self.cache_data.keys())[0])
+            first_key, _ = self.cache_data.popitem(False)
+            print("DISCARD: ", first_key)
 
     def get(self, key):
         """ Returns the value in self.cache_data with key"""
